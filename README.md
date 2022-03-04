9bb74946ebd9d5436677aca66a5f8be9417b02d8
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2022-03-04 17:24:17 -0300

    Subindo projeto no GitHub

---
+++ README.md
@@ -0,0 +1,6 @@
+# CounterApp
+
+- Aplicação mobile que consiste em um contador.
+- Desenvolvido durante a NLW#6 da <a href="https://github.com/Rocketseat">Rocketseat</a>.
+- Tecnologia utilizada:
+  - Flutter
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.blue), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Meu Primeiro App")),
+      body:
+          Center(child: Text("Contador\n$count", textAlign: TextAlign.center)),
+      floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.add),
+          onPressed: () {
+            increment();
+          }),
+    );
+  }
+}
